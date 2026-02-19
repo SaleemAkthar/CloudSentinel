9669e5197153a77e616dcc7b7e61c86d2ed10ee8
Saleem Akthar Ahmed <saleem.20240430@iit.ac.lk>
2026-02-19 15:30:18 +0530

    Updated  _inity_.py
    ran first test file it working perfectly

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,14 +1,3 @@
-"""
-Backend module for Serverless Anomaly Detection System
-"""
-
 from .online_detector import OnlineDetector, OnlineStats
-from .data_generator import generate_log_stream, generate_normal_log, generate_anomalous_log
 
-__all__ = [
-    'OnlineDetector',
-    'OnlineStats',
-    'generate_log_stream',
-    'generate_normal_log',
-    'generate_anomalous_log'
-]
+__all__ = ['OnlineDetector', 'OnlineStats']
