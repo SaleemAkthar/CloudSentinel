72d6bdbaf376469cf1b0824c84ffd6454d5046c1
Saleem Akthar Ahmed <saleem.20240430@iit.ac.lk>
2026-02-19 15:15:19 +0530

    New Test file Added

--- tests/test_online_stats.py
+++ tests/test_online_stats.py
@@ -1,31 +1,18 @@
 import unittest
 from backend.online_detector import OnlineStats
 
-class TestOnlineStats(unittest.TestCase):
-    def setUp(self):
-        self.stats = OnlineStats()
-
-    def test_initial_state(self):
-        self.assertEqual(self.stats.n, 0)
-        self.assertEqual(self.stats.get_mean(), 0.0)
-
-    def test_calculation_accuracy(self):
-        data = [10, 20, 30, 40, 50]
-        for x in data:
-            self.stats.update(x)
+class TestSaleemCore(unittest.TestCase):
+    def test_welford_accuracy(self):
+        stats = OnlineStats()
+        data = [100, 200, 300] # Mean = 200, Var = 10000, Std = 100
+        for val in data:
+            stats.update(val)
         
-        # Expected Mean: 30, Expected Variance: 250
-        self.assertEqual(self.stats.get_mean(), 30.0)
-        self.assertAlmostEqual(self.stats.get_variance(), 250.0)
-        self.assertAlmostEqual(self.stats.get_std(), 15.811388, places=5)
-
-    def test_z_score(self):
-        # Establish baseline
-        for x in [10, 10, 10, 10]: self.stats.update(x)
-        self.stats.update(20) # This is an outlier
+        self.assertEqual(stats.get_mean(), 200)
+        self.assertEqual(stats.get_std(), 100)
         
-        z = self.stats.get_z_score(20)
-        self.assertGreater(z, 1.0)
+        # Z-score for 400 should be 2.0
+        self.assertEqual(stats.get_z_score(400), 2.0)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
