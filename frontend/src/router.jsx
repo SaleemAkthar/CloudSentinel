d3fc10ceb5fa4fbbd7e90a6c13714ca516c0b281
Kithmini Bandhusena <kithmini.20240255@iit.ac.lk>
2026-03-09 04:47:07 +0530

    feat: add Notifications import to router

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import AppShell from "./components/Appshell";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
+import Notifications from "./pages/Notifications";
 import RealTimeAlerts from "./pages/RealTimeAlerts";
 import AIInsights from "./pages/AIInsights";
 import Team from "./pages/Team";
