320925c4963205bf41ea6a10b3fd355c4b4788ca
Kithmini Bandhusena <kithmini.20240255@iit.ac.lk>
2026-03-08 22:06:09 +0530

    feat: create Profile page skeleton

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,8 @@
+// Profile page component
+export default function Profile() {
+  return (
+    <div className="text-white">
+      <h1 className="text-3xl font-bold">My Profile</h1>
+    </div>
+  );
+}
