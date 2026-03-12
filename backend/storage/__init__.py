3cf7cb4546a4c489d885d138921c2d9af3a0df0e
Raneesha Ranasinghe <raneesharanasinghe@gmail.com>
2026-03-12 19:58:58 +0530

    chore : for now just set a placeholder

---
+++ backend/storage/__init__.py
@@ -0,0 +1,16 @@
+"""
+Storage Package
+===============
+
+In-memory and persistent storage for alerts and logs.
+
+Modules:
+- in_memory_store: AlertStore class for storing alerts
+"""
+
+# Will be uncommented when implemented
+# from .in_memory_store import AlertStore
+
+__all__ = [
+    # 'AlertStore',
+]
